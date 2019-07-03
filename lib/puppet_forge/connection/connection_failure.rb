c2e821e9eddc4d28d9d892635aba5b6b06b1fac5
Jesse Scott <jesse@puppetlabs.com>
2019-07-03 13:36:15 -0700

    (MAINT) Add more detail to connection failure messages

--- lib/puppet_forge/connection/connection_failure.rb
+++ lib/puppet_forge/connection/connection_failure.rb
@@ -9,11 +9,19 @@ module PuppetForge
         @app.call(env)
       rescue Faraday::ConnectionFailed => e
         baseurl = env[:url].dup
-        baseurl.path = ''
         if proxy = env[:request][:proxy]
-          errmsg = _("Unable to connect to %{url} (using proxy %{proxy})") % {url: baseurl.to_s, proxy: proxy.uri.to_s}
+          errmsg = _("Unable to connect to %{scheme}://%{host} (using proxy %{proxy}) (for request %{path_query})") % {
+            scheme: baseurl.scheme,
+            host: baseurl.host,
+            proxy: proxy.uri.to_s,
+            path_query: baseurl.request_uri,
+          }
         else
-          errmsg = _("Unable to connect to %{url}") % {url: baseurl.to_s}
+          errmsg = _("Unable to connect to %{scheme}://%{host} (for request %{path_query})") % {
+            scheme: baseurl.scheme,
+            host: baseurl.host,
+            path_query: baseurl.request_uri,
+          }
         end
         errmsg << ": #{e.message}"
         m = Faraday::ConnectionFailed.new(errmsg)
