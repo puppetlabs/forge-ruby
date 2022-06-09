puppetlabs/forge-ruby

commit 05a4e77279ddf6c66913016661099bc320edc1c5

(FORGE-575) Bump faraday dependency to 1.x range

puppet_forge.gemspec
@@ -18,13 +18,13 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
-  spec.required_ruby_version = '>= 1.9.3'
+  spec.required_ruby_version = '>= 2.4.0'
 
-  spec.add_runtime_dependency "faraday", [">= 0.9.0", "< 0.18.0", "!= 0.13.1"]
-  spec.add_runtime_dependency "faraday_middleware", [">= 0.9.0", "< 0.15.0"]
-  spec.add_dependency 'semantic_puppet', '~> 1.0'
-  spec.add_dependency 'minitar'
-  spec.add_dependency 'gettext-setup', '~> 0.11'
+  spec.add_runtime_dependency "faraday", "~> 1.3" 
+  spec.add_runtime_dependency "faraday_middleware", "~> 1.0" 
+  spec.add_dependency "semantic_puppet", "~> 1.0"
+  spec.add_dependency "minitar"
+  spec.add_dependency "gettext-setup", "~> 0.11"
 
   spec.add_development_dependency "rake"
   spec.add_development_dependency "rspec", "~> 3.0"

spec/unit/forge/connection_spec.rb
@@ -88,7 +88,7 @@ describe PuppetForge::Connection do
 
       expect {
         subject.get('/error')
-      }.to raise_error(Faraday::ClientError, "the server responded with status 503")
+      }.to raise_error(Faraday::ServerError, "the server responded with status 503")
     end
 
     context 'when an authorization value is provided' do

commit 66df7fe86c30b19f3530066378c59d8206191851

Remove redundant conditionals on RUBY_VERSION
The minimum version was declared as Ruby 2.4 so these conditionals do
not make sense.

Fixes: 05a4e77279ddf6c66913016661099bc320edc1c5

puppet_forge.gemspec
@@ -31,8 +31,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "cane"
   spec.add_development_dependency "yard"
   spec.add_development_dependency "redcarpet"
-
-  # Install the right pry debugging combo depending on ruby version.
-  spec.add_development_dependency "pry-debugger" if RUBY_VERSION <= '1.9.3'
-  spec.add_development_dependency "pry-byebug" if RUBY_VERSION >= '2.0.0'
+  spec.add_development_dependency "pry-byebug"
 end